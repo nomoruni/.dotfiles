2184c088942d91dd5ddb5d8c6841d0e194df2d6e
nomoruni <bryanvaldespino1999@gmail.com>
2023-09-15 15:30:11 -0400

    On branch master  Changes to be committed: 	modified:   gh/hosts.yml 	modified:   nvim/after/plugin/headlines.lua

--- nvim/after/plugin/headlines.lua
+++ nvim/after/plugin/headlines.lua
@@ -1,1 +1,13 @@
-require'headlines'.setup {}
+require('headlines').setup{
+	org = {
+		headline_highlights = { "Headline" },
+		codeblock_highlight = "CodeBlock",
+		dash_highlight = "Dash",
+		dash_string = "-",
+		quote_highlight = "Quote",
+		quote_string = "┃",
+		fat_headlines = true,
+		fat_headline_upper_string = "▃",
+		fat_headline_lower_string = "🬂",
+	},
+}
